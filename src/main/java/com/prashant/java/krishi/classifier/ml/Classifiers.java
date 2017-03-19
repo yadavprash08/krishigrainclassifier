f58f4604b7374c9df4297b4a5e688e4fbade9b40
Prashant Yadav <yprasha@amazon.com>
2017-03-19 01:31:38 -0700

    Adding the interface which will be used by the application for classifing the image attributes.

---
+++ src/main/java/com/prashant/java/krishi/classifier/ml/Classifiers.java
@@ -0,0 +1,19 @@
+package com.prashant.java.krishi.classifier.ml;
+
+import com.prashant.java.krishi.classifier.modal.wheat.WheatDimension;
+import lombok.NonNull;
+
+/**
+ * This is the basic interface which defines the contract on how would the classifiers respond with the results of
+ * the classifications.
+ */
+public interface Classifiers {
+    /**
+     * Classifies the two attributes for the wheat dimensions and produce the next dimension with populated class
+     * values.
+     *
+     * @param dimension {@link WheatDimension} which represents the attribute of the wheat grain image.
+     * @return WheatDimension with classified values.
+     */
+    WheatDimension classify(@NonNull WheatDimension dimension);
+}
